e0f58130b55d26e515b87945d20170803fa89ff9
durandnico <durandnico@cy-tech.fr>
2023-04-27 13:02:48 +0200

    débogueur

--- TD4/src/maths/polynome/Monome.java
+++ TD4/src/maths/polynome/Monome.java
@@ -1,2 +1,30 @@
-package maths.polynome;public class Monome {
+package maths.polynome;
+
+public class Monome {
+
+    private double coeff;
+    private int degre;
+
+    public Monome(double coeff, int degre){
+        this.coeff = coeff;
+        this.degre = degre;
+    }
+
+    public double getCoeff(){
+        return this.coeff;
+    }
+
+    public int getDegre(){
+        return this.degre;
+    }
+
+    public void setCoeff(double coeff){
+        this.coeff = coeff;
+    }
+
+    public void setDegre(int degre){
+        this.degre = degre;
+    }
+
+
 }
--- TD4/src/maths/polynome/Polynome.java
+++ TD4/src/maths/polynome/Polynome.java
@@ -2,46 +2,112 @@ package maths.polynome;
 
 public class Polynome{
 
-    private double[] coeffs;
+    private Monome[] monomes;
+    //private double[] coeffs;
 
-    public Polynome(double[] coeffs){
+    /*public Polynome(double[] coeffs){
         this.coeffs = coeffs;
+    }*/
+
+    public Polynome(Monome[] monomes){
+        this.monomes = monomes;
     }
 
-
-    public double[] getCoeffs(){
-        return this.coeffs;
+    public Monome[] getCoeffs(){
+        return this.monomes;
     }
 
     public double get(int degre){
-        if(degre > this.coeffs.length)
-            return 0;
 
-        return this.coeffs[degre];
+        for(Monome val : this.monomes)
+            if(val.getDegre() == degre)
+                return val.getCoeff();
+
+        return 0;
     }
 
-    public void setCoeff(int degre, double value){
-        if(degre > this.coeffs.length)
-            return;
-
-        this.coeffs[degre] = value;
-    }
 
     public int getDegre(){
-        return this.coeffs.length - 1;
-    }
-
-    public void toString(){
-        System.out.printf("%dx^%d", this.coeffs[this.coeffs.length - 1], this.coeffs.length - 1);
-        for(int i = this.coeffs.length - 2; i >= 0; --i){
-            System.out.printf(" %c %dx^%d", (this.coeffs[i] > 0) ? '+' : '-', this.coeffs[i], i);
+        int degre_max = 0;
+        for ( Monome val : this.monomes)
+        {
+            if(val.getDegre() > degre_max)
+                degre_max = val.getDegre();
         }
-        System.out.printf("\n");
+
+        return degre_max;
     }
 
+    public void show(){
+        for ( Monome val : this.monomes)
+        {
+            System.out.println(val.getCoeff() + "x^" + val.getDegre());
+        }
+    }
+
+    public boolean equals(Object o)
+    {
+        if (o instanceof Polynome)
+        {
+            Polynome p = (Polynome) o;
+            return this.getDegre() == p.getDegre() && equals(this, p, p.getDegre());
+        }
+
+        return false;
+    }
+
+    public static boolean equals(Polynome p, Polynome p2, int degre)
+    {
+        return degre < 0 || (p.get(degre) == p2.get(degre) && equals(p , p2, degre - 1) );
+    }
+
+    public double evaluate(double x)
+    {
+        double res = 0;
+        for (Monome val : this.monomes)
+        {
+            res += val.getCoeff() * Math.pow(x, val.getDegre());
+        }
+
+        return res;
+    }
+
+    public Polynome derive()
+    {
+        int new_deg = this.monomes.length;
+        for (Monome val : this.monomes)
+            if(val.getDegre() == 0)
+                new_deg--;
+
+
+        Polynome deriv = new Polynome(new Monome[new_deg]);
+        int index = 0;
+
+        for(Monome val : this.monomes)
+        {
+            if(val.getDegre() == 0)
+                continue;
+
+
+            deriv.monomes[index++] = (new Monome(val.getCoeff() * val.getDegre(), val.getDegre() - 1));
+        }
+
+        return deriv;
+    }
 
     public static void main(String[] args) {
-        Polynome P = new Polynome(1,2,3);
-        P.toString();
+        Monome[] monomes = new Monome[3];
+        monomes[0] = new Monome(1, 2);
+        monomes[1] = new Monome(2, 1);
+        monomes[2] = new Monome(3, 0);
+
+        Polynome p = new Polynome(monomes);
+        p.show();
+        System.out.println(p.getDegre());
+        System.out.println(p.get(2));
+        System.out.println(p.get(1));
+        System.out.println(p.get(0));
+        System.out.println(p.evaluate(2));
+        p.derive().show();
     }
 }
